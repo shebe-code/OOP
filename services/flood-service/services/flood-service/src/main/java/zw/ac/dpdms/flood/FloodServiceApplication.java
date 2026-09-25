d37a2860d77a64e406bd45086a0832a6c50dd7b6
shebe-code <shelaz926@gmail.com>
2026-09-25 12:43:23 +0200

    Add flood service application

---
+++ services/flood-service/services/flood-service/src/main/java/zw/ac/dpdms/flood/FloodServiceApplication.java
@@ -0,0 +1,11 @@
+package zw.ac.dpdms.flood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FloodServiceApplication.class, args);
+    }
+}
