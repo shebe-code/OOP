2508be3f875dfbc6b3b4cff1c93b2ad4b05ea771
shebe-code <shelaz926@gmail.com>
2026-09-25 19:04:23 +0200

    Add fire service application class

---
+++ services/fire-service/src/main/java/zw/ac/dpdms/fire/FireServiceApplication.java
@@ -0,0 +1,12 @@
+package zw.ac.dpdms.fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireServiceApplication.class, args);
+    }
+}
