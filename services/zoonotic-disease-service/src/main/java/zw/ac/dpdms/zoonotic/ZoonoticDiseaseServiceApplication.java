c0ec5a6cb0dc6a9c04eee1378cc083cf8a1b8c3d
shebe-code <shelaz926@gmail.com>
2026-09-25 19:12:24 +0200

    Add zoonotic disease application class

---
+++ services/zoonotic-disease-service/src/main/java/zw/ac/dpdms/zoonotic/ZoonoticDiseaseServiceApplication.java
@@ -0,0 +1,12 @@
+package zw.ac.dpdms.zoonotic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoonoticDiseaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoonoticDiseaseServiceApplication.class, args);
+    }
+}
