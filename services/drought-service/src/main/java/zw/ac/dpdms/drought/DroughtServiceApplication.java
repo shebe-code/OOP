299103001acafc782bf1c4bd38da71704dea8481
shebe-code <shelaz926@gmail.com>
2026-09-25 18:45:31 +0200

    Add drought service application class

---
+++ services/drought-service/src/main/java/zw/ac/dpdms/drought/DroughtServiceApplication.java
@@ -0,0 +1,12 @@
+package zw.ac.dpdms.drought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroughtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroughtServiceApplication.class, args);
+    }
+}
