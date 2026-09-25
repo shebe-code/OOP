ead00516768ea649e1225a21fa43c3e62b7f6b63
shebe-code <shelaz926@gmail.com>
2026-09-25 19:22:01 +0200

    Add mining accident service application class

---
+++ services/mining-accident-service/src/main/java/zw/ac/dpdms/mining/MiningAccidentServiceApplication.java
@@ -0,0 +1,12 @@
+package zw.ac.dpdms.mining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningAccidentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiningAccidentServiceApplication.class, args);
+    }
+}
